11b683236690621a901ff543eb0804538db357c2
manoj1995madushanka <madushankamanoj414@gmail.com>
2019-02-25 11:41:30 +0530

    chat servise

--- src/app/chat/chat.service.ts
+++ src/app/chat/chat.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {environment} from '../../environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
+  readonly token = environment.dialogflow.angularBot;
+  readonly client = new ApiAiClient({accessToken: this.token});
 
   constructor() { }
+
+  talk() {
+    this.client.textRequest('Who are you?').then(res => console.log(res));
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,10 @@
 
 export const environment = {
   production: false
+
+  dialogflow: {
+    angularBot: 'apiKey'
+  }
 };
 
 /*
